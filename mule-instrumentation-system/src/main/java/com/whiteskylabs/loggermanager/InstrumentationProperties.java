cae9559733eec084d58f02f9706bbeab0033a57c
Kalyan <kalyan.menda@whiteskylabs.com>
2014-09-15 16:51:23 +0530

    Created a generic notification class.
    Signed-off-by: Kalyan <kalyan.menda@whiteskylabs.com>

--- mule-instrumentation-system/src/main/java/com/whiteskylabs/loggermanager/InstrumentationProperties.java
+++ mule-instrumentation-system/src/main/java/com/whiteskylabs/loggermanager/InstrumentationProperties.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
-
 public class InstrumentationProperties {
-	
-	private static Logger log = Logger
-			.getLogger(InstrumentationProperties.class.getName());
 	
 	private String propertyFileName;
 	
@@ -24,14 +19,8 @@ public class InstrumentationProperties {
 
 	public String getPropValue(String key) throws IOException {
 	 
-	 String result = "";
 	 
      Properties prop = new Properties();
-     
-   //  String propFileName = "Instrumentation.properties";
-     
- //log.info("## the properties file name is ## "+propertyFileName);
-     
      
      String propFileName=propertyFileName.toString();
      
@@ -43,8 +32,6 @@ public class InstrumentationProperties {
      if (inputStream == null) {
          throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
      }
-     
-    // log.info("## the propertie value is  ##:"+prop.getProperty(key));
      
      return prop.getProperty(key);
 
